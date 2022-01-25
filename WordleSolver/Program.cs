7b63ae0eafcfaa95000e76bb6c6d4b1a4c325d2b
Adam Sasine <adam.sasine@gmail.com>
2022-01-25 11:58:11 -0500

    Changed input to ConsoleInput

--- WordleSolver/Program.cs
+++ WordleSolver/Program.cs
@@ -6,13 +6,7 @@
         {
             var words = new Words("../data/words_05_letters.txt", "../data/words_remove.txt");
             var consoleInput = new ConsoleInput();
-            var input = new FixedInput(new string[]
-            {
-                "rager",
-                "2 1 0 2 0",
-            }, true, consoleInput);
-
-            var game = new Game(words, input);
+            var game = new Game(words, consoleInput);
             game.Run();
         }
     }
